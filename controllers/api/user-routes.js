13572d8a45d356c0fc4f0cae580d5724cd41893e
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-10 22:33:25 -0500

    add all users route

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,4 +1,19 @@
 const router = require('express').Router();
 const { User, Rank, Friends} = require('../../models');
 
+// get users
+router.get('/', (req, res) => {
+    // access our user model and run .findAll() method)
+    User.findAll({
+        attributes: { exclude: ['password'] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
 module.exports = router;
