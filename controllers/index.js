71139bee0f090bd241ec35be1d33d6a63fad0c03
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-12 22:57:51 -0500

    create profile handlebars template file. add profile route to render hb profile template

--- controllers/index.js
+++ controllers/index.js
@@ -7,11 +7,16 @@ const homeRoutes = require('./home-routes.js');
 
 const gameRoutes = require('./game-routes.js');
 
+// grabs the user profile routes file
+const profileRoutes = require('./profile-routes');
+
 //sends the user to the homeroutes file when they go to the homepage
 router.use('/', homeRoutes);
 
 router.use('/game', gameRoutes);
 
 router.use('/api', apiRoutes);
+
+router.use('/', profileRoutes)
 
 module.exports = router;
