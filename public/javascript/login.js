d734976ed2ab69142c83c2932f6469a3a2b85649
JSheleg <jessica.sheleg@gmail.com>
2021-08-17 15:32:56 -0500

    added comments and comment form with style

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -13,7 +13,7 @@ async function loginFormHandler(event) {
         }),
         headers: { "Content-Type": "application/json" },
       });
-  
+      
       if (response.ok) {
         document.location.replace("/");
       } else {
