c3195b3e5e2fe933d31a8c079c0a692623376cba
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-19 18:08:53 -0500

    center unfollow btn

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -16,17 +16,17 @@ async function getFollowingIds(username) {
 }
 
 async function followingBtnHandler(btn) {
-  const username = btn.textContent;
+    const username = btn.textContent;
 
-  const response = await fetch(`/api/users/username/${username}`);
+    const response = await fetch(`/api/users/username/${username}`);
 
-  if (response.ok) {
-    const data = await response.json();
+    if (response.ok) {
+        const data = await response.json();
 
-    document.location.replace(`/profile/${data.id}`);
-  } else {
-    alert(response.statusText);
-  }
+        document.location.replace(`/profile/${data.id}`);
+    } else {
+        alert(response.statusText);
+    }
 }
 
 // run an if statement to check to see if the last word in the URL path isn't profile
@@ -37,8 +37,8 @@ const following_username = $('#profile-username').text();
 
 if (urlLastWord !== 'profile') {
     const followBtn = $('<button>')
-        .addClass('btn')
-        .text('+Follow')
+        .html("<i class='fas fa-user-plus'></i>")
+        .addClass('btn btn-primary')
         .attr({
             type: 'button',
             id: 'follow-btn'
@@ -108,15 +108,15 @@ let favoriteButton = document.getElementById("favorite-button");
 let toggleButton = "OFF";
 
 function toggle() {
-  let favoriteList = document.getElementById("favorite-game-list");
+    let favoriteList = document.getElementById("favorite-game-list");
 
-  if (toggleButton == "OFF") {
-    favoriteList.style.display = "none";
-    toggleButton = "ON";
-  } else if ((toggleButton = "ON")) {
-    favoriteList.style.display = "";
-    toggleButton = "OFF";
-  }
+    if (toggleButton == "OFF") {
+        favoriteList.style.display = "none";
+        toggleButton = "ON";
+    } else if ((toggleButton = "ON")) {
+        favoriteList.style.display = "";
+        toggleButton = "OFF";
+    }
 }
 
 favoriteButton.addEventListener("click", toggle);
@@ -125,15 +125,15 @@ let commentButton = document.getElementById("comment-button");
 let toggleCommentButton = "OFF";
 
 function commentToggle() {
-  let commentList = document.getElementById("comment-list");
-  if (toggleCommentButton == "OFF") {
-    commentList.style.display = "none";
+    let commentList = document.getElementById("comment-list");
+    if (toggleCommentButton == "OFF") {
+        commentList.style.display = "none";
 
-    toggleCommentButton = "ON";
-  } else if ((toggleCommentButton = "ON")) {
-    commentList.style.display = "";
-    toggleCommentButton = "OFF";
-  }
+        toggleCommentButton = "ON";
+    } else if ((toggleCommentButton = "ON")) {
+        commentList.style.display = "";
+        toggleCommentButton = "OFF";
+    }
 }
 
 commentButton.addEventListener("click", commentToggle);
@@ -143,15 +143,15 @@ let followButton = document.getElementById("follow-button");
 let toggleFollowList = "OFF";
 
 function followToggle() {
-  let followList = document.getElementById("following-list");
-  if (toggleFollowList == "OFF") {
-    followList.style.display = "none";
+    let followList = document.getElementById("following-list");
+    if (toggleFollowList == "OFF") {
+        followList.style.display = "none";
 
-    toggleFollowList = "ON";
-  } else if ((toggleFollowList = "ON")) {
-    followList.style.display = "";
-    toggleFollowList = "OFF";
-  }
+        toggleFollowList = "ON";
+    } else if ((toggleFollowList = "ON")) {
+        followList.style.display = "";
+        toggleFollowList = "OFF";
+    }
 }
 
 followButton.addEventListener("click", followToggle);
