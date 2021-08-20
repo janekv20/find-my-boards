852f6d78984371ffdbc886ed7eaada6468020a82
Joshua Sands <josh.sands@mail.com>
2021-08-19 20:40:46 -0500

    heroku reset 1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret:'super secret secret',
+  secret: process.env.SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
