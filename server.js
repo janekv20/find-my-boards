ce3c7679beb1fe86fe59b2abd6ec41facea758dd
Mjgiannelli <51857101+mjgiannelli@users.noreply.github.com>
2021-08-20 18:57:05 -0500

    fix bug where upload post wasn't going through on heroku. updated where pic files are being saved and called. added static middleware to give hb access to controllers/profile-pics folder

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'controllers')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(fileUpload());
